2758e0777fa4237a29b36371c6297756e3e75d1c
Your Name <you@example.com>
2022-02-10 09:17:41 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ev_tapsirigi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            string[] words = { "Bmw", "Hyundai", "Toyota", "Changan" };
+            foreach (string word in words)
+            {
+                {
+                    if (word.Length > 5)
+                        Console.WriteLine(word);
+                }
+
+
+
+
+
+            }
+        }
+    }
+}
